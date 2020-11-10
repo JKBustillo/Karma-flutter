ca857f66e2d84dd29b5d0e721ce3082ac1c3f3fa
Jose Bustillo <josebustillodealba@gmail.com>
2020-11-09 20:31:10 -0500

    feat: login page first

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:karma_flutter/widgets/login.dart';
-import 'package:karma_flutter/widgets/register.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Register(),
+      home: Login(),
     ),
   );
 }
